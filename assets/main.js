2b92c4b9c33949a7659ff50c5db1af75b5d42e21
Tomio Mizoroki <tmizoroki@gmail.com>
2017-12-25 22:37:53 -0800

    Create getKeyCodeToAudio function
    Write a function that takes the audioConfig array and uses it to create a map of the keyCodes and the associated Audio element.

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,10 @@
 import audioConfig from './audioConfig.js';
+
+const keyCodeToAudio = getKeyCodeToAudio();
+
+function getKeyCodeToAudio() {
+  return audioConfigs.reduce((keyCodeToAudio, config) => {
+    keyCodeToAudio[config.keyCode] = new Audio(config.src);
+    return keyCodeToAudio;
+  }, {});
+}
