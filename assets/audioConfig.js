5a6cd0b809519cfa405b8b5d0ce3655d57d4122c
Tomio Mizoroki <tmizoroki@gmail.com>
2017-12-25 22:28:15 -0800

    Create a config file to organize the associations of the key codes and audio files

---
+++ assets/audioConfig.js
@@ -0,0 +1,13 @@
+const audioConfig = [
+  { keyCode: 65, src: 'sounds/clap.wav' }, // a
+  { keyCode: 83, src: 'sounds/hihat.wav' }, // s
+  { keyCode: 68, src: 'sounds/kick.wav' }, // d
+  { keyCode: 70, src: 'sounds/openhat.wav' }, // f
+  { keyCode: 71, src: 'sounds/boom.wav' }, // g
+  { keyCode: 72, src: 'sounds/ride.wav' }, // h
+  { keyCode: 74, src: 'sounds/snare.wav' }, // j
+  { keyCode: 75, src: 'sounds/tom.wav' }, // k
+  { keyCode: 76, src: 'sounds/tink.wav' } // l
+];
+
+export default audioConfig;
