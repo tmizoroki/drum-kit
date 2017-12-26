f97fbe62ee8106a1b45ba148854173b1bedb7be7
Tomio Mizoroki <tmizoroki@gmail.com>
2017-12-25 22:44:00 -0800

    Fix path to audio files

--- audioConfig.js
+++ audioConfig.js
@@ -1,13 +1,13 @@
 const audioConfig = [
-  { keyCode: 65, src: 'sounds/clap.wav' }, // a
-  { keyCode: 83, src: 'sounds/hihat.wav' }, // s
-  { keyCode: 68, src: 'sounds/kick.wav' }, // d
-  { keyCode: 70, src: 'sounds/openhat.wav' }, // f
-  { keyCode: 71, src: 'sounds/boom.wav' }, // g
-  { keyCode: 72, src: 'sounds/ride.wav' }, // h
-  { keyCode: 74, src: 'sounds/snare.wav' }, // j
-  { keyCode: 75, src: 'sounds/tom.wav' }, // k
-  { keyCode: 76, src: 'sounds/tink.wav' } // l
+  { keyCode: 65, src: 'assets/sounds/clap.wav' }, // a
+  { keyCode: 83, src: 'assets/sounds/hihat.wav' }, // s
+  { keyCode: 68, src: 'assets/sounds/kick.wav' }, // d
+  { keyCode: 70, src: 'assets/sounds/openhat.wav' }, // f
+  { keyCode: 71, src: 'assets/sounds/boom.wav' }, // g
+  { keyCode: 72, src: 'assets/sounds/ride.wav' }, // h
+  { keyCode: 74, src: 'assets/sounds/snare.wav' }, // j
+  { keyCode: 75, src: 'assets/sounds/tom.wav' }, // k
+  { keyCode: 76, src: 'assets/sounds/tink.wav' } // l
 ];
 
 export default audioConfig;
