c5f416825d69f54e2a42a890be9946e23ae05ba3
Tomio Mizoroki <tmizoroki@gmail.com>
2017-12-25 22:46:04 -0800

    Fix typo

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import audioConfig from './audioConfig.js';
 const keyCodeToAudio = getKeyCodeToAudio();
 
 function getKeyCodeToAudio() {
-  return audioConfigs.reduce((keyCodeToAudio, config) => {
+  return audioConfig.reduce((keyCodeToAudio, config) => {
     keyCodeToAudio[config.keyCode] = new Audio(config.src);
     return keyCodeToAudio;
   }, {});
