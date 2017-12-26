2db89523fac04bd632f32c6aae1784fadffb5c06
Tomio Mizoroki <tmizoroki@gmail.com>
2017-12-25 22:46:49 -0800

    Add keydown event listener and log keyCodes
    Add a keydown event listener to the window object and write the
    callback function so that it logs the key codes from the event
    objects.

--- main.js
+++ main.js
@@ -2,9 +2,16 @@ import audioConfig from './audioConfig.js';
 
 const keyCodeToAudio = getKeyCodeToAudio();
 
+window.addEventListener('keydown', handleKeydown);
+
 function getKeyCodeToAudio() {
   return audioConfig.reduce((keyCodeToAudio, config) => {
     keyCodeToAudio[config.keyCode] = new Audio(config.src);
     return keyCodeToAudio;
   }, {});
+}
+
+function handleKeydown(event) {
+  const keyCode = event.keyCode;
+  console.log(keyCode);
 }
